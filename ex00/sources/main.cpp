c5f90443c94d7ec58b2f9bca7ca2b658311d8401
Pierrick NEAU <pierrickn44@gmail.com>
2024-04-01 17:22:18 +0200

    main updated

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 13:37:18 by pineau            #+#    #+#             */
-/*   Updated: 2024/03/12 14:18:22 by pineau           ###   ########.fr       */
+/*   Updated: 2024/04/01 16:17:01 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,35 @@ int main() {
 	int b = 2;
 	double c = 1.5;
 	double d = 2.5;
+	std::string s1 = "chaine1";
+	std::string s2 = "chaine2";
 
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
 	swap(a, b);
 	swap(c, d);
+	swap(s1, s2);
+	std::cout << "-----SWAP-----" <<std::endl;
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << std::endl;
+	std::cout << "s1 = " << s1 << ", s2 = " << s2 << std::endl;
 	
 	int int_min = min(a, b);
 	double double_min = min(c, d);
+	std::string string_min = min(s1, s2);
 	
+	std::cout << "-----MIN-----" <<std::endl;
 	std::cout << "int_min = " << int_min << std::endl;
 	std::cout << "double_min = " << double_min << std::endl;
-	std::cout << std::endl;
+	std::cout << "string_min = " << string_min << std::endl;
 	
 	int int_max = max(a, b);
 	double double_max = max(c, d);
+	std::string string_max = max(s1, s2);
 
+	std::cout << "-----MAX-----" <<std::endl;
 	std::cout << "int_max = " << int_max << std::endl;
 	std::cout << "double_max = " << double_max << std::endl;
+	std::cout << "string_max = " << string_max << std::endl;
 }
-
